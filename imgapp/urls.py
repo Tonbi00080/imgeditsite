04d2b24294323f6c54f337829f805d7bb5a78553
高山雄貴 <yt1219g@gmail.com>
2019-03-05 17:21:23 +0900

    初期画面設定、ファイルのアップロード機能

--- imgapp/urls.py
+++ imgapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'imgapp'
 urlpatterns = [
     path('', views.index,name='index'),
+    path('upload', views.upload,name='upload'),
+    path('preview', views.preview,name='preview'),
+    path('complete', views.complete,name='complete')
 ]
--- imgapp/views.py
+++ imgapp/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from .forms import ImageForm
+from .models import Image
 
 # Create your views here.
 def index(request):
     return render(request,'imgapp/index.html')
+
+def upload(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            # ファイルの保存
+            form.save()
+            obj = Image.objects.all()
+            import pdb; pdb.set_trace()
+            return render(request,'imgapp/preview.html',{
+            'form': form,
+            'obj': obj
+            })
+    else:
+        form = ImageForm()
+    return render(request, 'imgapp/upload.html',{'form': form})
+
+def preview(request):
+    return render(request,'imgapp/preview.html')
+
+def complete(request):
+    return render(request,'imgapp/complete.html')
