b3a768bdfed51c00cac082d0a6229be10603ff6e
高山雄貴 <yt1219g@gmail.com>
2019-03-16 19:36:18 +0900

    モデルの項目名修正

--- imgapp/models.py
+++ imgapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Image(models.Model):
-    name = models.CharField(primary_key=True, max_length=255)
-    cvt_type = models.CharField(max_length=255)
-    img= models.FileField(upload_to='img/')
-    edittedImg= models.FileField(upload_to='edittedImg/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(primary_key=True, max_length=255, verbose_name='画像名')
+    cvt_type = models.CharField(max_length=255, verbose_name='編集タイプ')
+    img= models.FileField(upload_to='img/', verbose_name='加工したい画像')
+    edittedImg= models.FileField(upload_to='edittedImg/', verbose_name='加工した画像')
+    uploaded_at = models.DateTimeField(default=timezone.now)
